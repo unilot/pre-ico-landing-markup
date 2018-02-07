b0e7cc0f9dae8bcb78095ab1c1f571d0d516b046
Alegzander <alexander.aka.alegz@gmail.com>
2018-02-07 19:53:29 +0600

    Fixes for bioep.

--- assets/js/form-validation.js
+++ assets/js/form-validation.js
@@ -54,202 +54,207 @@
 
         var formFinalSettings = $.extend(defaultSettings, formSettings);
 
-        $forms.validator({
-            custom: {
-                wallet: function ($input) {
-                    if ($input.val().length > 0 && !(new Web3()).isAddress($input.val())) {
-                        var error = config.messages.errors.validation.invalidWallet;
+        $forms.each(function (index, formElement) {
+            var $_singleForm = $(formElement);
 
-                        if ($input.data('wallet-error')) {
-                            error = $input.data('wallet-error');
-                        } else if ($input.data('error')) {
-                            error = $input.data('error');
+            $_singleForm.validator({
+                focus: !$_singleForm.hasClass('js-form-no-focus'),
+                custom: {
+                    wallet: function ($input) {
+                        if ($input.val().length > 0 && !(new Web3()).isAddress($input.val())) {
+                            var error = config.messages.errors.validation.invalidWallet;
+
+                            if ($input.data('wallet-error')) {
+                                error = $input.data('wallet-error');
+                            } else if ($input.data('error')) {
+                                error = $input.data('error');
+                            }
+
+                            return error;
+                        }
+                    },
+                    online: function ($input) {
+                        var url = $input.data('online');
+                        var $form = $input.closest('form');
+                        var validator = $form.data('bs.validator');
+                        var data = {};
+
+                        if ($input.val().trim().length === 0) {
+                            return false;
                         }
 
-                        return error;
+                        data[$input.attr('name')] = $input.val();
+
+                        //TODO a lot of code duplication. This part should probably be refactored
+                        if (validator && ($input.data('bs.validator.errors') || []).length === 0) {
+                            $.ajax({
+                                url: url,
+                                dataType: 'json',
+                                method: 'OPTIONS',
+                                xhrFields: {
+                                    withCredentials: true
+                                },
+                                headers: {
+                                    'X-CSRFToken': Cookies.get('csrftoken')
+                                },
+                                success: function () {
+                                    $.ajax({
+                                        url: url,
+                                        dataType: 'json',
+                                        data: data,
+                                        method: 'POST',
+                                        xhrFields: {
+                                            withCredentials: true
+                                        },
+                                        headers: {
+                                            'X-CSRFToken': Cookies.get('csrftoken')
+                                        },
+                                        statusCode: {
+                                            400: function (jqXHR, textStatus, errorThrown) {
+                                                var errors = $input.data('bs.validator.errors');
+                                                try {
+                                                    var resposneData = JSON.parse(jqXHR.responseText);
+                                                    errors.push(resposneData[$input.attr('name')]);
+                                                } catch (e) {
+                                                    errors.push(jqXHR.responseText || textStatus);
+                                                }
+
+                                                $input.data('bs.validator.errors', errors);
+                                                validator.showErrors($input);
+                                            }
+                                        },
+                                        error: function (jqXHR, textStatus, errorThrown) {
+                                            if (jqXHR.status === 400)
+                                                return true;
+
+                                            var error = config.messages.errors.validation.requestFailed;
+
+                                            $input.data('bs.validator.errors', [error]);
+                                            validator.showErrors($input);
+                                        }
+                                    });
+                                },
+                                error: function (jqXHR, textStatus, errorThrown) {
+                                    var error = config.messages.errors.validation.requestFailed;
+
+                                    $input.data('bs.validator.errors', [error]);
+                                    validator.showErrors($input);
+                                }
+                            });
+                        }
+                    },
+                    phone: function ($input) {
+                        var phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();
+                        var $form = $input.closest('form');
+                        var validator = $form.data('bs.validator');
+
+                        try {
+                            var phone = phoneUtil.parse('+' + $input.val().replace(/^\D+/g, ''));
+
+                            if (!phoneUtil.isValidNumber(phone)) {
+                                throw new Error();
+                            }
+
+                        } catch (exception) {
+                            return $input.data('error');
+                        }
                     }
-                },
-                online: function ($input) {
-                    var url = $input.data('online');
-                    var $form = $input.closest('form');
-                    var validator = $form.data('bs.validator');
-                    var data = {};
+                }
+            }).on('submit', function (event) {
+                if (event.isDefaultPrevented()) {
+                    return false;
+                }
 
-                    if ($input.val().trim().length === 0) {
-                        return false;
-                    }
+                var _self = this;
+                var $form = $(_self);
+                var hasOverlay = false;
+                var $overlayedBlock = null;
+                var $overlay = null;
 
-                    data[$input.attr('name')] = $input.val();
+                if (( $overlayedBlock = $form.hasClass('has-overlay') )) {
+                    hasOverlay = true;
+                } else if (( $overlayedBlock = $form.closest('.has-overlay') ).length > 0) {
+                    hasOverlay = true;
+                }
 
-                    //TODO a lot of code duplication. This part should probably be refactored
-                    if (validator && ($input.data('bs.validator.errors') || []).length === 0) {
+                $overlay = $('.overlay', $overlayedBlock);
+
+                if (hasOverlay) {
+                    $overlay.removeClass('hidden');
+                }
+
+                var data = form2Data($form);
+                event.preventDefault();
+
+                $.ajax({
+                    url: $form.prop('action'),
+                    dataType: 'json',
+                    method: 'OPTIONS',
+                    xhrFields: {
+                        withCredentials: true
+                    },
+                    headers: {
+                        'X-CSRFToken': Cookies.get('csrftoken')
+                    },
+                    success: function () {
                         $.ajax({
-                            url: url,
+                            url: $form.prop('action'),
                             dataType: 'json',
-                            method: 'OPTIONS',
+                            data: data,
+                            method: $form.attr('method'),
                             xhrFields: {
                                 withCredentials: true
                             },
                             headers: {
                                 'X-CSRFToken': Cookies.get('csrftoken')
                             },
-                            success: function () {
-                                $.ajax({
-                                    url: url,
-                                    dataType: 'json',
-                                    data: data,
-                                    method: 'POST',
-                                    xhrFields: {
-                                        withCredentials: true
-                                    },
-                                    headers: {
-                                        'X-CSRFToken': Cookies.get('csrftoken')
-                                    },
-                                    statusCode: {
-                                        400: function (jqXHR, textStatus, errorThrown) {
-                                            var errors = $input.data('bs.validator.errors');
-                                            try {
-                                                var resposneData = JSON.parse(jqXHR.responseText);
-                                                errors.push(resposneData[$input.attr('name')]);
-                                            } catch (e) {
-                                                errors.push(jqXHR.responseText || textStatus);
-                                            }
+                            statusCode: {
+                                403: function (jqXHR, testStatus, errorThrown) {
+                                    window.location.reload();
+                                },
+                                400: function (jqXHR, textStatus, errorThrown) {
+                                    try {
+                                        $.each(JSON.parse(jqXHR.responseText), function (fieldName, errors) {
+                                            var $input = $('[name="' + fieldName + '"]', $form);
 
                                             $input.data('bs.validator.errors', errors);
-                                            validator.showErrors($input);
-                                        }
-                                    },
-                                    error: function (jqXHR, textStatus, errorThrown) {
-                                        if (jqXHR.status === 400)
-                                            return true;
+                                            $form.data('bs.validator').showErrors($input);
+                                        });
 
-                                        var error = config.messages.errors.validation.requestFailed;
-
-                                        $input.data('bs.validator.errors', [error]);
-                                        validator.showErrors($input);
+                                        formFinalSettings.badRequest($form, jqXHR, textStatus, errorThrown);
+                                    } catch (e) {
+                                        formFinalSettings.fail.call(_formProcessor, $form, jqXHR, textStatus, errorThrown);
                                     }
-                                });
+
+                                }
                             },
                             error: function (jqXHR, textStatus, errorThrown) {
-                                var error = config.messages.errors.validation.requestFailed;
-
-                                $input.data('bs.validator.errors', [error]);
-                                validator.showErrors($input);
+                                formFinalSettings.fail.call(_formProcessor, $form, jqXHR, textStatus, errorThrown);
+                            },
+                            complete: function () {
+                                if (hasOverlay) {
+                                    $overlay.addClass('hidden');
+                                }
+                            },
+                            success: function () {
+                                formFinalSettings.valid.call(_formProcessor, $form);
                             }
                         });
-                    }
-                },
-                phone: function ($input) {
-                    var phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();
-                    var $form = $input.closest('form');
-                    var validator = $form.data('bs.validator');
-
-                    try {
-                        var phone = phoneUtil.parse('+' + $input.val().replace( /^\D+/g, ''));
-
-                        if (!phoneUtil.isValidNumber(phone)) {
-                            throw new Error();
+                    },
+                    error: function (jqXHR, textStatus, errorThrown) {
+                        if (hasOverlay) {
+                            $overlay.addClass('hidden');
                         }
 
-                    } catch (exception) {
-                        return $input.data('error');
+                        formFinalSettings.fail.call(_formProcessor, $form, jqXHR, textStatus, errorThrown);
+                    },
+                    beforeSend: function (jqXHR, settings) {
+                        formFinalSettings.beforeSend.call(_formProcessor, $form, jqXHR, settings)
                     }
-                }
-            }
-        }).on('submit', function (event) {
-            if (event.isDefaultPrevented()) {
+                });
+
                 return false;
-            }
-
-            var _self = this;
-            var $form = $(_self);
-            var hasOverlay = false;
-            var $overlayedBlock = null;
-            var $overlay = null;
-
-            if (( $overlayedBlock = $form.hasClass('has-overlay') )) {
-                hasOverlay = true;
-            } else if (( $overlayedBlock = $form.closest('.has-overlay') ).length > 0) {
-                hasOverlay = true;
-            }
-
-            $overlay = $('.overlay', $overlayedBlock);
-
-            if (hasOverlay) {
-                $overlay.removeClass('hidden');
-            }
-
-            var data = form2Data($form);
-            event.preventDefault();
-
-            $.ajax({
-                url: $form.prop('action'),
-                dataType: 'json',
-                method: 'OPTIONS',
-                xhrFields: {
-                    withCredentials: true
-                },
-                headers: {
-                    'X-CSRFToken': Cookies.get('csrftoken')
-                },
-                success: function () {
-                    $.ajax({
-                        url: $form.prop('action'),
-                        dataType: 'json',
-                        data: data,
-                        method: $form.attr('method'),
-                        xhrFields: {
-                            withCredentials: true
-                        },
-                        headers: {
-                            'X-CSRFToken': Cookies.get('csrftoken')
-                        },
-                        statusCode: {
-                            403: function (jqXHR, testStatus, errorThrown) {
-                                window.location.reload();
-                            },
-                            400: function (jqXHR, textStatus, errorThrown) {
-                                try {
-                                    $.each(JSON.parse(jqXHR.responseText), function (fieldName, errors) {
-                                        var $input = $('[name="' + fieldName + '"]', $form);
-
-                                        $input.data('bs.validator.errors', errors);
-                                        $form.data('bs.validator').showErrors($input);
-                                    });
-
-                                    formFinalSettings.badRequest($form, jqXHR, textStatus, errorThrown);
-                                } catch (e) {
-                                    formFinalSettings.fail.call(_formProcessor, $form, jqXHR, textStatus, errorThrown);
-                                }
-
-                            }
-                        },
-                        error: function (jqXHR, textStatus, errorThrown) {
-                            formFinalSettings.fail.call(_formProcessor, $form, jqXHR, textStatus, errorThrown);
-                        },
-                        complete: function () {
-                            if (hasOverlay) {
-                                $overlay.addClass('hidden');
-                            }
-                        },
-                        success: function () {
-                            formFinalSettings.valid.call(_formProcessor, $form);
-                        }
-                    });
-                },
-                error: function (jqXHR, textStatus, errorThrown) {
-                    if (hasOverlay) {
-                        $overlay.addClass('hidden');
-                    }
-
-                    formFinalSettings.fail.call(_formProcessor, $form, jqXHR, textStatus, errorThrown);
-                },
-                beforeSend: function (jqXHR, settings) {
-                    formFinalSettings.beforeSend.call(_formProcessor, $form, jqXHR, settings)
-                }
             });
-
-            return false;
         });
     };
 
